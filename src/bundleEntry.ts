7c61810eb62de7ddd745d60fd64124b684715539
Andres Sewell <sewellandres@gmail.com>
2021-10-23 15:27:32 -0400

    Prevent mod execution if Repentance is not installed

--- src/bundleEntry.ts
+++ src/bundleEntry.ts
@@ -1,6 +1,9 @@
 // Entry point for the TypeScriptToLua bundler
 import main from "./main";
 
-main();
+// If the user does not have Repentance installed, don't proceed
+if (REPENTANCE === true) {
+  main();
+}
 
 // Do not add any code to this file
