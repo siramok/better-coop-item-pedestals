adf939c70c2784f8c04fef75e7c2286d777f8a33
Andres Sewell <sewellandres@gmail.com>
2021-10-24 21:10:28 -0400

    Fix lint.sh and package.json to avoid a ci issue

--- lint.sh
+++ lint.sh
@@ -22,7 +22,6 @@ npx eslint --max-warnings 0 src
 
 # Step 2 - Spell check every file using cspell
 # We use no-progress and no-summary because we want to only output errors
-npx cspell --no-progress --no-summary "src/**/*.ts"
-npx cspell --no-progress --no-summary "mods/metadata.xml"
+npx cspell --no-progress "src/**/*.ts"
 
 echo "Success!"
